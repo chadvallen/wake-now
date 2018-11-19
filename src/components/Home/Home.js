fe44f4e1a289a3a39e0af046cc113a8578b7c420
Chad Allen <chadvallen@gmail.com>
2018-11-18 17:58:53 -0700

    added mobile home image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../../media/white-logo.png';
 import wake from '../../media/wake.png';
 import now from '../../media/now.png';
+import together from '../../media/together.png';
 import '../../App';
 
 export default function Home() {
@@ -11,6 +12,9 @@ export default function Home() {
                 <img className ="wake" src={wake} alt="wake" />
                 <img className="main-logo" src={logo} alt="logo" />
                 <img className="now" src={now} alt="now" />
+            </div>
+            <div>
+                <img className="together" src={together} alt="logo" />
             </div>
         </div>
     )
